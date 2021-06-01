4c3bb097d122499e0a662c32041d1013064ec197
sreeragps <sreerag@brainchild.services>
2021-06-01 18:11:48 +0530

    Mainpage component created and added to App component.

---
+++ food-hub-app/src/components/Mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Mainpage=()=>{
+
+    return(
+        <div className="mainpage-container">
+        <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default Mainpage;
